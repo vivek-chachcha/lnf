440a82f8176cf769cbdfd86e78311babe4bfad21
Mandy Fung <frozenera@hotmail.com>
2015-10-27 17:07:42 -0700

    issue #2 Added base html template, home and about pages are done

--- LNF/views.py
+++ LNF/views.py
@@ -1,3 +1,12 @@
+from django.template import RequestContext, loader
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.views.generic import View
 
-# Create your views here.
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home.html')
+		
+class AboutView(View):
+    def get(self, request):
+        return render(request, 'about.html')
