60399ce8a01b4701071de1f4e7cb1cf3bf75892a
Mandy Fung <frozenera@hotmail.com>
2015-10-30 21:47:08 -0700

    issue #10 separated lost and found posts into separate pages

--- LNF/urls.py
+++ LNF/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^posts/$', views.PostView.as_view(), name='posts'),
+    url(r'^lostposts/$', views.LostPostView.as_view(), name='lostposts'),
+    url(r'^foundposts/$', views.FoundPostView.as_view(), name='foundposts'),
 ]
--- LNF/views.py
+++ LNF/views.py
@@ -5,11 +5,19 @@ from django.views.generic import View
 
 from .models import Post
 
-class PostView(View):
+class LostPostView(View):
     model = Post
-    template_name = 'posts/posts.html'
 
     def get(self, request):
-        post_list = Post.objects.all()
-        context = {'post_list': post_list}
-        return render(request, 'posts/posts.html', context)
+        lost_post_list = Post.objects.filter(state='Lost')
+        context = {'lost_post_list': lost_post_list}
+        return render(request, 'posts/lostposts.html', context)
+		
+class FoundPostView(View):
+    model = Post
+
+    def get(self, request):
+        found_post_list = Post.objects.filter(state='Found')
+        context = {'found_post_list': found_post_list}
+        return render(request, 'posts/foundposts.html', context)
+
