a27401bf654fcb52b4313b43a9130b11127c60c5
jshe <jennifershe123@gmail.com>
2015-11-02 20:53:22 -0800

    edited date lost/found

--- LNF/views.py
+++ LNF/views.py
@@ -5,7 +5,6 @@ from LNF.forms import UserCreateForm, LoginForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from django.core.urlresolvers import reverse
 from .models import Post
 from .forms import PostForm
 from PIL import Image
